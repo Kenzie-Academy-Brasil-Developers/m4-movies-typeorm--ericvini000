a782210a72e4cf7678a6e70b14daecc1fab92656
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-08-21 12:11:02 -0400

    feat: base files created

---
+++ src/data-source.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+import "dotenv";
+import "express-async-errors";
+import path from "path";
+
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "entities/**.{js,ts}");
+  const migrationsPath: string = path.join(__dirname, "migrations/**.{js,ts}");
+
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+  if (!dbUrl) throw new Error("Missing database URL in .env");
+
+  if (process.env.NODE_ENV == "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      logging: true,
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    url: dbUrl,
+    logging: true,
+    synchronize: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const AppDataSouce: DataSource = new DataSource(dataSourceConfig());
+
+export default AppDataSouce;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import AppDataSource from "./data-source";
+
+AppDataSource.initialize()
+  .then(() => {
+    const PORT: number = Number(process.env.PORT) || 3000;
+    app.listen(PORT, () => {
+      console.log("Server is running");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
