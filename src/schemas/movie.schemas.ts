413471bf220b8c1e2edd8c6452a3a4f40be23b31
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-08-23 15:52:22 -0400

    feat: entity description changed

--- src/schemas/movie.schemas.ts
+++ src/schemas/movie.schemas.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const movieSchema = z.object({
   id: z.number().positive(),
   name: z.string().max(50),
-  description: z.string().nullable(),
+  description: z.string().nullish(),
   duration: z.number(),
   price: z.number(),
 });
